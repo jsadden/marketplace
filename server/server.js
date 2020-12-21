b9a0b7b364d8a2022f0f61076fcb8dc198f1988b
Jim <jsadden8@gmail.com>
2020-12-21 15:21:46 -0500

    'deploy_config'

--- server/server.js
+++ server/server.js
@@ -27,6 +27,14 @@ mongoose.connect(process.env.MONGODB_URI, {
     useFindAndModify: false
 })
 
+//production config
+app.use(express.static('client/build'))
+if(process.env.NODE_ENV === 'production') {
+    const path = require('path')
+    app.get('/*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
